2ef1864df8c7719b104b56ba6604e81dea2ba5d0
Zachary Mayfield <zakmayfield@gmail.com>
2019-12-11 17:47:53 -0500

    added some stylying to my page, will have to redo styles tomorrow for mvp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-import TitleDate from './components/TitleDate';
+import Title from './components/Title';
+import Date from './components/Date';
 import ImageDisplay from './components/ImageDisplay';
 import Explain from './components/Explain';
 import axios from 'axios';
@@ -7,6 +8,7 @@ import "./App.css";
 
 function App() {
   const [photoData, setPhotoData] = useState([]);
+  // cosnt [specificDate, setSpecificDate] = useState('')
 
   useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=hJK5m8gB7G78sk77SdkOLysES6qGL2PL7smjjfLy`)
@@ -21,23 +23,48 @@ function App() {
   return (
     <div className="container">
 
-      <div className="logo-title-container">
-        <h1>NASA</h1>
-        {photoData.map((item, index) => {
-          return <TitleDate key={index} date={item.date} title={item.title} />
-        })}
-      </div>
 
+    {/* HEADER CONTAINER THE LOGO / TITLE OF PHOTO / AND DATE  */}
+      <div className="header">
+        <div className="logo-container">
+          <h1>NASA</h1>
+        </div>
+
+        <div className="title-container">
+          {photoData.map((item, index) => {
+            return <Title key={index} title={item.title} />
+          })}
+        </div>
+
+        <div className="date-container">
+          {photoData.map((item, index) => {
+            return <Date key={index} date={item.date} />
+          })}
+        </div>
+      </div>
+    {/* END OF HEADER */}
+
+
+    {/* IMAGE CONTAINER */}
       <div className="image-container">
         {photoData.map((item, index) => {
           return <ImageDisplay key={index} hdurl={item.hdurl} />
         })}
       </div>
+    {/* IMAGE CONTAINER ENDS */}
 
+    
+    {/* DESCRIPTION CONTAINER */}
       <div className="description-container">
         {photoData.map((item, index) => {
           return <Explain key={index} explanation={item.explanation} />
         })}
+      </div>
+    {/* DESCRIPTION CONTAINER ENDS */}
+
+    {/* FOOTER CONTAINER */}
+      <div className="footer-container">
+        <p>All rights reserved by NASA</p>
       </div>
 
     </div>
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = (props) => {
+  return (
+    <div className="sub-title-container">
+      <p>{props.title}</p>
+    </div>
+  )
+}
+
+export default Title
